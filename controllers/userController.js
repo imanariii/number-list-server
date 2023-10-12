b1b35e00221cb5b5cf7205f4bba3802c9d51a997
imanariii <imanariii>
2023-10-12 10:33:20 +0300

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,78 +1,24 @@
-const ApiError = require('../error/ApiError');
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const {User, Product } = require('../models/models')
-const { isEmail } = require("validator");
-
-const generateJwt = (id, email, role) => {
-    return jwt.sign(
-        {id, email, role},
-        process.env.SECRET_KEY,
-        {expiresIn: '24h'}
-    )
-}
-
+const {readFileSync} = require("fs");
+const db = readFileSync('db.json', 'utf8');
 class UserController {
-    async registration(req, res, next) {
-        const {email, password, role, address} = req.body
-        console.log(email, password, role, address)
-        if (!email || !password) {
-            return next(ApiError.badRequest('Некорректный email или password'))
-        }
-        const candidate = await User.findOne({where: {email}})
-        if (candidate) {
-            return next(ApiError.badRequest('Пользователь с таким email уже существует'))
-        }
-        if (!email.includes('@')) {
-            return next(ApiError.badRequest('Некорректный email'))
-        }
-        if (password.length < 6) {
-            return next(ApiError.badRequest('Длина password должна быть больше 6 символов'))
-        }
-        if (email.length < 6) {
-            return next(ApiError.badRequest('Длина email должна быть больше 6 символов'))
-        }
-        const hashPassword = await bcrypt.hash(password, 5)
-        const user = await User.create({email, role, password: hashPassword, address: address})
-        const token = generateJwt(user.id, user.email, user.role, user.address)
-        return res.json({token})
-    }
+    async getAll(req, res) {
+        const arrDB = JSON.parse(db)
 
-    async login(req, res, next) {
-        const {email, password} = req.body
-        const user = await User.findOne({where: {email}})
-        if (!user) {
-            return next(ApiError.internal('Пользователь не найден'))
+        const {email, number} = req.body
+
+        let newNumber = ''
+
+        if(number !== null) {
+            number.split('-').forEach(item => {
+                newNumber += item
+            })
         }
-        let comparePassword = bcrypt.compareSync(password, user.password)
-        if (!comparePassword) {
-            return next(ApiError.internal('Указан неверный пароль'))
-        }
-        const token = generateJwt(user.id, user.email, user.role, user.address)
-        return res.json({token})
-    }
 
-    async check(req, res, next) {
-        const token = generateJwt(req.user.id, req.user.email, req.user.role, req.user.address)
-        return res.json({token})
-    }
+        const result = arrDB.filter(item => number !== null ?
+            item.email === email && item.number === newNumber :
+            item.email === email)
 
-    async update(req, res, next) {
-        let {address} = req.body
-        const {id} = req.params
-
-        const user = await User.update({ address },
-            {
-                where: {
-                    id: id,
-                },
-            }
-        )
-        return res.json(user)
-    }
-    async getAll(req, res, next) {
-        const users = await User.findAll()
-        return res.json(users)
+        setTimeout(()=>res.json(result), 5000)
     }
 }
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,7 @@
 const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userController')
-const authMiddleware = require('../middleware/authMiddleware')
 
-router.post('/registration', userController.registration)
-router.post('/login', userController.login)
-router.get('/auth', authMiddleware, userController.check)
-router.get('/', userController.getAll)
+router.post('/', userController.getAll)
 
 module.exports = router
